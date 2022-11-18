18f7602e83d12eabcbfd648606625429d17380de
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-17 23:18:31 -0300

    criado tela de tecnologia

--- src/app/tecnologias/tecnologias.component.ts
+++ src/app/tecnologias/tecnologias.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tecnologias',
   templateUrl: './tecnologias.component.html',
-  styleUrls: ['./tecnologias.component.css']
+  styleUrls: ['./tecnologias.component.css'],
 })
 export class TecnologiasComponent implements OnInit {
+  ImageBootstrap: string;
+  Imagejavascript: string;
+  Imagereact: string;
+  Imageangular: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.ImageBootstrap = '/assets/img/bootstrap.png';
+    this.Imagejavascript = '/assets/img/javascript.png';
+    this.Imagereact = '/assets/img/react.png';
+    this.Imageangular = '/assets/img/eangular.png';
   }
 
+  ngOnInit(): void {}
 }
